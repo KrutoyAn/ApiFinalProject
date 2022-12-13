05175ca7ef6817a42ad0708b8394b8fea1d4d594
Andrei Krutoy <andrei.krutoy1997@gmail.com>
2022-12-13 22:46:58 +0300

    rewrite constants

--- app/src/main/java/com/hfad/apifinalproject/MainActivity.kt
+++ app/src/main/java/com/hfad/apifinalproject/MainActivity.kt
@@ -12,7 +12,9 @@ class MainActivity : AppCompatActivity() {
     private companion object {
 
         private const val INTERVAL = 100L
-        private const val PERIOD = 1000L * 30
+        private const val PERIOD = 1000L * 10
+        private const val PERIOD_VIEW_TWO = 1000L * 30
+        private const val PERIOD_VIEW_THREE = 1000L * 35
         private const val REPEAT = 10
     }
     private lateinit var binding: ActivityMainBinding
@@ -24,8 +26,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.customViewOne.setPeriod(PERIOD)
-        binding.customViewTwo.setPeriod(PERIOD)
-        binding.customViewThree.setPeriod(PERIOD)
+        binding.customViewTwo.setPeriod(PERIOD_VIEW_TWO)
+        binding.customViewThree.setPeriod(PERIOD_VIEW_THREE)
 
         lifecycleScope.launch {
             while (current < PERIOD * REPEAT) {
